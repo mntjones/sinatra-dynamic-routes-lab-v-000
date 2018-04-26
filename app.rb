9d9eb1c3ead04d5f7b1f903535ae58252e3cfb71
Monica Tamaru Jones <mntamaru@gmail.com>
2018-04-26 20:03:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   
   get '/square/:number' do
     @number = params[:number].to_i
-    "#{@number^2}"
+    "#{@number * @number}"
   end
   
   get '/say/:number/:phrase' do
